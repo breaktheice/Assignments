443c7a697a34c77e8bc91cd1ce69ccc4b0a20f74
UUsha <usha.umakanthan@gmail.com>
2017-03-03 01:06:22 -0800

    LinkedList.java-Usha

--- LinkedList.java
+++ LinkedList.java
@@ -7,21 +7,69 @@ public class LinkedList {
 
     public class Node {
         //WRITE YOUR CODE HERE
-
-        //WRITE YOUR CLASS DEFINITION HERE
+    	 //WRITE YOUR CLASS DEFINITION HERE
+    	int data;
+    	Node next;
+    	public void setData(int data){
+    		
+    		this.data = data;
+       	}
+    	
+   public int getData(){
+	   return data;
+   }
+   
+   public void setNext(Node next){
+		
+		this.next = next;
+  	}
+   public Node getNext(){
+	   return next;
+   }
 
         //WRITE YOUR CONSTRUCTORS HERE
     }
     Node head;
+    Node temp;
 
     //print
     public void printList(){
         //WRITE YOUR CODE HERE
+    temp=head;
+    while(temp!=null){
+    	
+    		System.out.println(temp.getData());
+    		temp =temp.getNext();
+       	}
+    	
     }
 
     //add
+    
     public void addNode(int data){
         //WRITE YOUR CODE HERE
+    	Node node = new Node();
+    	node.setData(data);
+    	if(head == null){
+    		
+    		head=node;
+    		
+    	}
+      	else{
+    		
+    		temp= head;
+    		//temp.setNext(node);
+    		while (temp.getNext()!=null) {
+    			
+    			temp =temp.getNext();
+    			   			    			
+    		}
+    		temp.setNext(node);
+    		   		
+    	}   	
+   	
+    	
+    	
     }
 
     //reverse
@@ -29,14 +77,21 @@ public class LinkedList {
         //WRITE YOUR CODE HERE
 
         //base case
+    	if(currNode.getNext() == null){
+    		head= currNode;
+    		return ;
+    	}
 
         //RECURSION
-        reverseList(currNode.next);
+    	Node secondNode = currNode.getNext();
+        reverseList(secondNode);
+        secondNode.setNext(currNode);
+        currNode.setNext(null);
     }
 
     //test
     public static void testBench(){
-        LinkedList reverseList = new LinkedList();
+       LinkedList reverseList = new LinkedList();
         reverseList.addNode(1);
         reverseList.addNode(2);
         reverseList.addNode(3);
@@ -47,11 +102,13 @@ public class LinkedList {
 
         reverseList.printList();
         System.out.println("");
-        reverseList.reverseList(reverseList.head);
+        System.out.println("before");
+       reverseList.reverseList(reverseList.head);
+       
         reverseList.printList();
     }
     public static void main(String[] args){
-        testBench();
+       testBench();
+    	
     }
 }
-
