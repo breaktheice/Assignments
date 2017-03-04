8edf432cd8ac298946def495416ba5c00ce28938
Vinoth Jeevanandam <vinoth.j@gmail.com>
2017-03-04 10:26:21 -0800

    Updated NumbersCount using Recursion -Vinoth

--- NumbersCount.java
+++ NumbersCount.java
@@ -2,6 +2,7 @@ package IceBreakers;
 import java.util.*;
 public class NumbersCountRec {
 	String str;
+	int[] tmp = new int[10];
     public NumbersCountRec(String str) {
         this.str= str;
     }
@@ -11,7 +12,7 @@ public class NumbersCountRec {
     
     
     public void findCount() {
-    	
+    	System.out.println("\n\nInput String: " + str);
     	if(str.isEmpty() || (! str.matches("\\d+"))) {
     		System.out.println("Invalid Input");
     		return;
@@ -20,51 +21,59 @@ public class NumbersCountRec {
     	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     	int arr[] = new int[10];
     	int num;
+    	int max = Integer.MIN_VALUE;
     	for(int i=0;i<str.length();i++) {
     		num = Character.getNumericValue(str.charAt(i));
     		if(map.containsKey(num)) {
     			map.put(num,  map.get(num)+1);
+    			if(map.get(num) > max) {
+    				max = map.get(num);
+    			}
     		} else {
     			map.put(num, 1);
     		}
+    		
 			arr[num] = map.get(num);
     	}
+
+    	System.out.println();
+
+    	drawChart(arr, max);
     	for(int i=0;i<arr.length;i++) {
     		System.out.print(i + " ");
-    	}
+    	}    	
     	System.out.println();
-    	
-    	drawChart(arr, 0);
-    	
     }
     
-    void drawChart(int[] arr, int flag) {
-
+    void drawChart(int[] arr, int max) {
     	for(int i=0;i<arr.length;i++) {
-    		if(arr[i] != 0) {
+    		if(arr[i] == max) {
     			System.out.print("# ");
     			arr[i]--;
-    			flag = 1;
     		} else {
     			System.out.print("  ");
     		}
+    		
     	}
     	System.out.println();
-    	if(flag == 0) return;
-    	drawChart(arr, 0);
+    	if(max == 0) return;
+    	drawChart(arr, max-1);
     }
+
     public static void main(String[] args) {
-        NumbersCountRec numbersCount = new NumbersCountRec("933262154439441526816992388562667004907159682643816");
-    	//NumbersCountRec numbersCount = new NumbersCountRec("2146859296389521599993229915608941463961565182");
-    	//NumbersCountRec numbersCount = new NumbersCountRec("2342523423432141324");
-    	NumbersCountRec numbersCount1 = new NumbersCountRec("");
+        NumbersCountRec numbersCount1 = new NumbersCountRec("933262154439441526816992388562667004907159682643816");
+    	NumbersCountRec numbersCount2 = new NumbersCountRec("2146859296389521599993229915608941463961565182");
+    	NumbersCountRec numbersCount3 = new NumbersCountRec("2342523423432141324");
+    	NumbersCountRec numbersCount4 = new NumbersCountRec("");
+    	NumbersCountRec numbersCount5 = new NumbersCountRec("abc");
+    	NumbersCountRec numbersCount6 = new NumbersCountRec();
     	
-    	NumbersCountRec numbersCount2 = new NumbersCountRec("abc");
-    	NumbersCountRec numbersCount3 = new NumbersCountRec();
-    	
-        numbersCount.findCount();
         numbersCount1.findCount();
         numbersCount2.findCount();
         numbersCount3.findCount();
+        numbersCount4.findCount();
+        numbersCount5.findCount();
+        numbersCount6.findCount();
+        
     }
 }
