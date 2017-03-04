1a279ada8be988152c85799a0f8f6fde38dddaac
Vinoth Jeevanandam <vinoth.j@gmail.com>
2017-03-04 11:01:25 -0800

    Updated the code without using HashMap
    Thanks mouly for the tip. Let me know if i can improve my algorithm further.

--- NumbersCount.java
+++ NumbersCount.java
@@ -77,24 +77,14 @@ public class NumbersCountRec {
     		return;
     	}
 
-    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
     	int arr[] = new int[10];	
     	int num;
     	int max = Integer.MIN_VALUE;
     	for(int i=0;i<str.length();i++) {
     		num = Character.getNumericValue(str.charAt(i));
-    		if(map.containsKey(num)) {
-    			map.put(num,  map.get(num)+1);
-    			if(map.get(num) > max) {
-    				max = map.get(num);
-    			}
-    		} else {
-    			map.put(num, 1);
-    		}
-    		
-			arr[num] = map.get(num);
+    		arr[num]++;
+    		max = Integer.max(arr[num], max);
     	}
-
     	System.out.println();
 
     	drawChart(arr, max);
@@ -117,22 +107,21 @@ public class NumbersCountRec {
     	System.out.println();
     	if(max == 0) return;
     	drawChart(arr, max-1);
+    	
     }
-
+    
     public static void main(String[] args) {
         NumbersCountRec numbersCount1 = new NumbersCountRec("933262154439441526816992388562667004907159682643816");
-    	NumbersCountRec numbersCount2 = new NumbersCountRec("2146859296389521599993229915608941463961565182");
-    	NumbersCountRec numbersCount3 = new NumbersCountRec("2342523423432141324");
-    	NumbersCountRec numbersCount4 = new NumbersCountRec("");
-    	NumbersCountRec numbersCount5 = new NumbersCountRec("abc");
-    	NumbersCountRec numbersCount6 = new NumbersCountRec();
-    	
         numbersCount1.findCount();
-        numbersCount2.findCount();
-        numbersCount3.findCount();
-        numbersCount4.findCount();
-        numbersCount5.findCount();
-        numbersCount6.findCount();
-        
+    	NumbersCountRec numbersCount2 = new NumbersCountRec("2146859296389521599993229915608941463961565182");
+    	numbersCount2.findCount();
+    	NumbersCountRec numbersCount3 = new NumbersCountRec("2342523423432141324");
+    	numbersCount3.findCount();
+    	NumbersCountRec numbersCount4 = new NumbersCountRec("");
+    	numbersCount4.findCount();
+    	NumbersCountRec numbersCount5 = new NumbersCountRec("abc");
+    	numbersCount5.findCount();
+    	NumbersCountRec numbersCount6 = new NumbersCountRec();
+    	numbersCount6.findCount();
     }
 }
