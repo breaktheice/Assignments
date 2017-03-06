099ea2385c2b854fd3ec3bb8aae114a9d569ccea
Vinoth Jeevanandam <vinoth.j@gmail.com>
2017-03-05 19:07:45 -0800

    DistinctElements - Solved - Vinoth
    If the input elements are in single digit from 0 -9, this solution will work.

--- class6/DistinctElements.java
+++ class6/DistinctElements.java
@@ -1,13 +1,16 @@
+package IceBreakers;
 
-/**
- * Created by IceBreakers on 3/5/17.
- * if an input array has distinct elements, return TRUE, otherwise false
- *  input = [3,4,6,2,1,6,8] - False
- *  input = [2,3,1,7,6] - True
- */
 public class DistinctElements {
     private static boolean isDistinct(int[] nums){
-
+    	int[] res = new int[10];
+    	for(int i=0;i<nums.length;i++) {
+    		if(res[nums[i]] != 0) {
+    			return false;
+    		} else {
+    			res[nums[i]] = 1;
+    		}
+    	}
+    	return true;
     }
 
     public static void testBench(){
@@ -15,6 +18,11 @@ public class DistinctElements {
         // 1 element array
         // array with distinct elements case
         // array with not distinct elements case
+    	System.out.println(isDistinct(new int[] {3,4,6,2,1,6,8}));
+    	System.out.println(isDistinct(new int[] {1,2,3,4,5,6,1}));
+    	System.out.println(isDistinct(new int[] {2,3,1,7,6}));
+    	System.out.println(isDistinct(new int[] {1}));
+    	System.out.println(isDistinct(new int[] {}));
     }
     public static void main(String args[]){
         testBench();
