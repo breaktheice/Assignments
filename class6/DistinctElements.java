a2bffa88409122a9a13fb83a45e6539dbca3b70e
UUsha <usha.umakanthan@gmail.com>
2017-03-07 00:00:06 -0800

    DistinctElements.java-usha
    Array { } is null =true
    Array { 2 } is Distinct =true
    Array {2,5,8,9,1,3} is Distinct =true
    Array {2,5,8,9,2,1,3} is Distinct =false

--- class6/DistinctElements.java
+++ class6/DistinctElements.java
@@ -7,14 +7,44 @@
  */
 public class DistinctElements {
     private static boolean isDistinct(int[] nums){
+    	int arrayLength = nums.length;
+    	if (arrayLength == 0){
+    		return true;
+       	}
+    	boolean isDistinct = true;
+    	int[] count = new int[10];
+    	for(int i=0 ; i < nums.length; i++){
+    		
+    		if(nums[i] >=0 || nums[i] <=9){
+    			
+    			int index = nums[i];
+    			count[index]++;
+    			if(count[index] > 1){
+    				isDistinct = false;
+    				break;
+    			}
+    			
+    		
+    		}
+    	}
+    	return isDistinct;
 
     }
 
     public static void testBench(){
         //null case
+    	int[] nums = { };
+       	System.out.println("Array { } is null =" +isDistinct(nums));
+    	
         // 1 element array
+    	int[] nums1 = { 2 };    	
+    	System.out.println("Array { 2 } is Distinct =" +isDistinct(nums1));
         // array with distinct elements case
+    	int[] nums2 = {2,5,8,9,1,3};    	
+    	System.out.println("Array {2,5,8,9,1,3} is Distinct =" +isDistinct(nums2));
         // array with not distinct elements case
+    	int[] nums3 = {2,5,8,9,2,1,3};    	
+    	System.out.println("Array {2,5,8,9,2,1,3} is Distinct =" +isDistinct(nums3));
     }
     public static void main(String args[]){
         testBench();
